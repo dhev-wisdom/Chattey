1cc9c8863568660293c602124bcda6ccf2c40d8e
dhev-wisdom <nonsowisdom62@gmail.com>
2023-10-18 18:38:47 +0100

    check heroku deploy success

--- chattey/settings.py
+++ chattey/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import dj_database_url
 import django_heroku
+import channels.layers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
