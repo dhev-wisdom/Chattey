0bef13ee39ca32a22b6a720af539644cf6c64c10
dhev-wisdom <nonsowisdom62@gmail.com>
2023-10-19 17:16:27 +0100

    Check

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: daphne chattey.asgi:application --port $PORT --bind 0.0.0.0
+web: daphne chattey.asgi:application --port $PORT --bind 0.0.0.0 -v2
+chatteyworker: python manage.py runworker --setting=chattey.setting -v2
--- chattey/settings.py
+++ chattey/settings.py
@@ -4,28 +4,20 @@ import dj_database_url
 import django_heroku
 import channels.layers
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['chattey-ebd43eb5547a.herokuapp.com', '127.0.0.1:8000']
 
 
-# Application definition
-
 INSTALLED_APPS = [
     # 'djongo'
     'channels',
     'base',
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
