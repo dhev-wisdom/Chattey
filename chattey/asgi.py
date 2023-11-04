180b855f23fb2bc94a8759e04509de6708344987
dhev-wisdom <nonsowisdom62@gmail.com>
2023-11-04 22:03:24 +0100

    check deploy success

--- chattey/asgi.py
+++ chattey/asgi.py
@@ -6,11 +6,10 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from django.setup import setup
 from channels.auth import AuthMiddlewareStack
 from base.routing import websocket_urlpatterns
 
-setup()
+django.setup()
 
 django_asgi_app = get_asgi_application()
 
