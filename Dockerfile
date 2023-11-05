e7fb5c4b6f00731d3c5cf83ef36bd4db591198eb
dhev-wisdom <nonsowisdom62@gmail.com>
2023-11-05 12:27:01 +0100

    check deploy success

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,13 @@ RUN apt-get update && apt-get install -y build-essential
 RUN pip install --upgrade pip
 
 COPY requirements.txt /app/
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app
+
+COPY static /app/static
+
+RUN python manage.py collectstatic --noinput
+
 CMD ["daphne", "-b", "0.0.0.0", "chattey.asgi:application"]
--- base/routing.py
+++ base/routing.py
@@ -7,6 +7,4 @@ django.setup()
 websocket_urlpatterns = [
     path('wss/private-chat/<str:username>/', PrivateChatConsumer.as_asgi()),
     path('wss/group-chat/<int:id>/', GroupChatConsumer.as_asgi()),
-    # re_path(r"ws/private-chat/(P<user_id>\d+)/$", PrivateChatConsumer.as_asgi()),
-    # re_path(r"ws/group-chat/(P<group_id>\d+)/$", GroupChatConsumer.as_asgi())
 ]
